64fa170f7090e60219129ea03b2209b45873e52b
ale_maj <maggioalessio2@gmail.com>
2022-06-17 18:13:28 +0200

    Prototype Npc patrol behavior
    Prototype npc patrol behavior: waypoints.

--- UnityFundamentals3D/Assets/Scripts/NPCController.cs
+++ UnityFundamentals3D/Assets/Scripts/NPCController.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NPCController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float patrolTime = 10f;
+    public float aggroRange = 10f;
+    public Transform[] waypoints;   //collection of data
 
-    // Update is called once per frame
-    void Update()
+    //without access modifier(public, private... = private)
+    int index;  //what index we're on our waypoints arr
+    float speed, agentSpeed;    //what current speed is and what component is
+    Transform player;   //need to know where the player it is
+
+    Animator anim;
+    NavMeshAgent agent;
+
+    void Awake()
     {
-        
+        //anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent != null)
+        {
+            agentSpeed = agent.speed;
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length);
+
+        InvokeRepeating(nameof(Tick), 0, 0.5f);
+
+        if (waypoints.Length > 0)
+        {
+            InvokeRepeating(nameof(Patrol), 0, patrolTime);
+        }
+    }
+    void Patrol() //set the index we re using
+    {
+        index = index == waypoints.Length - 1 ? 0 : index + 1;
+    }
+    void Tick() 
+    {
+        agent.destination = waypoints[index].position;
     }
 }
