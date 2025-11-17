3a4369520b765a7d683cc3f5843d8a0437d2e25b
ale maj <45163764+Sudo-Ale@users.noreply.github.com>
2025-11-17 02:11:10 +0100

    Background repeat, Obstacles spawn and destroy, Animations
    - background repeat
    - obstacle stop spawn when player collide with
    - obstacle destroyed when off set
    - animation running (default), jumping, death

--- Create with Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -3,27 +3,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+
     public float jumpForce = 10f;
     public float gravityModifier;
+
     public bool isOnGround = true;
+    public bool gameOver;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            // jump force up
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+
+            // animation trigger jump
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround= true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } 
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
+            Debug.Log("Game Over");
+        }
     }
 }
--- Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,16 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new(30, 0, 0);
 
     public float startDelay = 2;
-    public float repeatRate = 1.5f;
+    public float minRange = 1f;
+    public float maxRange = 3f;
+
+    private PlayerController playerController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
         //InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         StartCoroutine(SpawnObstacleRoutine());
     }
@@ -20,12 +25,13 @@ public class SpawnManager : MonoBehaviour
     private IEnumerator SpawnObstacleRoutine()
     {
         yield return new WaitForSeconds(startDelay);
+
         while (true)
         {
             SpawnObstacle();
 
-            // Wait random seconds between 1, 3 seconds
-            float waitTime = UnityEngine.Random.Range(1f, 3f);
+            // Wait random seconds between min, max range seconds
+            float waitTime = UnityEngine.Random.Range(minRange, maxRange);
             yield return new WaitForSeconds(waitTime);
         }
     }
@@ -38,6 +44,9 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnObstacle() 
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerController.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
